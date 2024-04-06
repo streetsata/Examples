6bf468b4b9330048cc3be85ba754efbd8000262c
Serhii Artemenko <sergartem.sa@gmail.com>
2024-04-06 22:57:58 +0300

    db added

---
+++ WebApiSqlServerDockerDemo/WebApiSqlServerDockerDemo/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApiSqlServerDockerDemo.Data;
+using WebApiSqlServerDockerDemo.Models;
+
+namespace WebApiSqlServerDockerDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController(OnlineShopDbContext context) : ControllerBase
+    {
+        private readonly OnlineShopDbContext _context = context;
+
+        [HttpGet]
+        public async Task<IEnumerable<Product>> Get()
+        {
+            return await _context.Products.ToListAsync();
+        }
+    }
+}
